d12acf174ef16db988eb8a93281c8a627c2733de
Timo Senechal <t.senechal@epages.com>
2019-03-28 15:51:00 +0100

    Add checkout link for clerk

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -3,13 +3,21 @@
     <div class="card-body">
       <h5 class="card-title">{{ product.name }}</h5>
       <div class="card-text">
-        <img class="card-img-top img-thumbnail p-0 m-0" :src="imageLink" :alt="product.name">
+        <a :href="checkoutLink(product._id)"><img class="card-img-top img-thumbnail p-0 m-0" :src="imageLink" :alt="product.name"></a>
         <div v-if="!product.variations || variations.length" class="qr-codes">
           QR Codes:
           <a
             v-for="qrcode in qrcodes"
             :href="qrcode.href"
             :key="qrcode.href"
+          >{{qrcode.name}}</a>
+        </div>
+        <div v-if="!product.variations || variations.length" class="checkout-links">
+          Add to cart:
+          <a
+            v-for="qrcode in qrcodes"
+            :href="qrcode.checkoutHref"
+            :key="qrcode.checkoutHref"
           >{{qrcode.name}}</a>
         </div>
       </div>
@@ -43,7 +51,8 @@ export default {
         return [
           {
             name: this.product.name,
-            href: this.getQrCodeLink(this.product._id)
+            href: this.getQrCodeLink(this.product._id),
+            checkoutHref: this.checkoutLink(this.product._id)
           }
         ];
       }
@@ -52,7 +61,8 @@ export default {
           name: variation.variationAttributeValues
             .map(value => value.value)
             .join(" "),
-          href: this.getQrCodeLink(variation._id)
+          href: this.getQrCodeLink(variation._id),
+          checkoutHref: this.checkoutLink(variation._id)
         }));
       }
     },
@@ -73,13 +83,16 @@ export default {
         this.variations = product.variations;
       }
     },
-    getQrCodeLink: function(productId) {
-      var addToCartTemplate =
-        "https://beyond-instore-checkout.herokuapp.com/{shop}/cart/{productId}";
-      var addToCartLink = uriTemplates(addToCartTemplate).fill({
+    checkoutLink(productId) {
+    var addToCartTemplate =
+      "https://beyond-instore-checkout.herokuapp.com/{shop}/cart/{productId}";
+      return uriTemplates(addToCartTemplate).fill({
         shop: this.$route.params.shop,
-        productId: productId
+        productId
       });
+    },
+    getQrCodeLink: function(productId) {
+      var addToCartLink = this.checkoutLink(productId)
       var qrCodeTemplate =
         "https://api.qrserver.com/v1/create-qr-code/?{&size,format,data}";
       return uriTemplates(qrCodeTemplate).fill({
@@ -106,7 +119,8 @@ export default {
   height: 80%;
 }
 
-.qr-codes a {
+.qr-codes a,
+.checkout-links a {
   padding: 5px;
 }
 </style>
